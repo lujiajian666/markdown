15bf71b5e3a26f164599f0374f803bc552c5f72e
lujiajian <lujj@corp.21cn.com>
2020-06-21 15:47:19 +0800

    feat: 主动更改高度

--- src/main.js
+++ src/main.js
@@ -13,10 +13,33 @@ const contentPromise = axios({
 document.addEventListener('DOMContentLoaded',function(){
   setTimeout(() => {
     contentPromise.then(res => {
-      console.log(res);
-      document.getElementById('content').innerHTML = marked(res.data);
+      const contentDom = document.getElementById('content');
+      const markdownText = marked(res.data);
+      contentDom.innerHTML = markdownText; 
+      // 统计img加载
+      let imgLoadCount = 0;
+      const imgList = [...document.body.innerHTML.matchAll('<img')];
+      if (imgList.length) {
+        contentDom.querySelectorAll('img').forEach(item => {
+          item.addEventListener('load', () => {
+            imgLoadCount++;
+            if (imgList.length <= imgLoadCount) {
+              resetHeight();
+            }
+          })
+        })
+      } else {
+        // 没图片情况
+        resetHeight();
+      }
     })
-  }, 1000)
+  }, 700)
 });
+
+function resetHeight () {
+  const iframe = window.parent.document.querySelector('#md-iframe');
+  const height = document.documentElement.scrollHeight || document.body.scrollHeight;
+  iframe.style.height = height + 'px';
+}
 
 
